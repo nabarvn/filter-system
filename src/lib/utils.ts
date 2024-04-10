e355ab1707abea2a9465507b8fa614a39f4b82f8
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-04-10 23:59:36 +0530

    Add support for generating absolute URLs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { type ClassValue, clsx } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function absoluteUrl(path: string) {
+  if (typeof window !== "undefined") return path;
+  if (process.env.BASE_URL) return `https://${process.env.BASE_URL}${path}`;
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
